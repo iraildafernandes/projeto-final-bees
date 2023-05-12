03de9cb94da34b1458bf62431353a1553abe17ab
iraildafernandes <iraildafernandes@live.com>
2023-05-12 18:07:57 -0300

    first commit

---
+++ src/Components/Contato/index.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './styles.css';
+
+function Contato() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  return (
+    <div className="container">
+      <h2 className="title">Contato</h2>
+
+      <form className="form" onSubmit={() => {}}>
+        <input 
+          className="input"
+          type="text"
+          placeholder="Digite seu nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        
+        <input 
+          className="input"
+          type="text"
+          placeholder="Digite seu email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+
+        <textarea 
+          className="textarea"
+          placeholder="Digite sua mensagem..."
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+
+        <input className="button" type="submit" value="Enviar" />
+      </form>
+
+    </div>
+  );
+}
+
+export default Contato;
