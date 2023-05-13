e103d53615f5da1fe52f352fca5d971f1143ac99
Linda <lindadaniellyo@gmail.com>
2023-05-13 00:43:56 -0300

    Commit-Linda

---
+++ src/Paginas/Pagina3/pagina3.jsx
@@ -0,0 +1,46 @@
+import './pagina3.css'
+
+
+//header
+
+<main className="principal">
+
+<div id="titulo1">
+<h2>Mulheres que brilham</h2>
+</div>
+
+<div className="blocos-videos">
+
+<div className="bloco">
+    <div className="video"><iframe width="560" height="315" src="https://www.youtube.com/embed/q7ncvX68fLA" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <p>Ada, a mãe da programação.</p>
+    </div>
+
+    <div><iframe width="560" height="315" src="https://www.youtube.com/embed/TWuGOLHer0o" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <p>Conheça Girls Who Code CEO Dr. Tarika Barrett.</p></div>
+
+    <div><iframe width="560" height="315" src="https://www.youtube.com/embed/3Y8a3ko_sV0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <p> Katherine Johnson, a cientista negra que ajudou a Apolo 11 a chegar à Lua.</p>
+    </div>
+</div>
+
+<div className="bloco">
+    <div><iframe width="560" height="315" src="https://www.youtube.com/embed/Kd-TvkHj_aA" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <p>Bell hooks (1952-2021) foi uma pensadora, professora, escritora<br/> e ativista negra norte-americana de grande importância,<br/> principalmente para o movimento antirracista e feminista.</p>
+    </div>
+
+    <div><iframe width="560" height="315" src="https://www.youtube.com/embed/zfI8A9ZDrNI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <p>Mulheres e meninas negras em Tech!</p>
+    </div>
+
+    <div><iframe width="560" height="315" src="https://www.youtube.com/embed/YBnNvETQBJ8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <p>PretaLab, uma organização que busca incentivar a <br/> participação de mulheres negras na área de <br/>tecnologia por meio de cursos e mentorias.</p>
+    </div>
+</div>
+
+
+</div>
+
+</main>
+
+//footer
